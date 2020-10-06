1e991832235d5e905e926c19532a990bfd914a7d
baraa96 <34281301+baraa96@users.noreply.github.com>
2020-10-06 13:49:55 +0300

    Update - Add File of new Course - Java

---
+++ Code/Intellij/SDK_07_2020_Sec06_Java/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.awt.*;
+import java.lang.reflect.Array;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        // primitive dataType
+        int firstNumber = 1000000;
+        float secondNumber = 100.36f;
+        double doubleNumber = 10.36f;
+        char charLetter = 'A';
+        boolean isNumber = true;
+        short shortNumber = 32000;
+        long longNumber = 1000000000;
+
+        // non - primitive dataType
+        String name = "Jon Smith";
+        Array arrayData ;
+        List listData;
+
+        System.out.println("My Name is " + name); // concatenation
+
+    }
+}
